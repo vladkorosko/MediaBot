5d1b3462f03a05d36191ba1e33c58101ea3ff809
vladkorosko <vlad.korosko1@gmail.com>
2023-01-26 10:42:26 +0200

    Conversion .avi to .mp4

---
+++ video_processing/format_conversion.py
@@ -0,0 +1,19 @@
+from moviepy.editor import *
+import os
+
+
+def convert_avi_to_mp4_ffmpeg(avi_file_path, result_file_path):
+    # if os.path.exists(result_file_path):
+    #     os.remove(result_file_path)
+    try:
+        os.popen("ffmpeg -i {0} -c:v mpeg4 -preset fast -crf 19 {1}".format(avi_file_path, result_file_path))
+        return True
+    except:
+        return False
+
+
+def convert_avi_to_mp4_moviepy(avi_file_path, result_file_path):
+    clip = VideoFileClip(avi_file_path)
+    clip.write_videofile(result_file_path)
+
+
