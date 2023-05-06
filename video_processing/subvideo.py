61f5588032144febad76289b21965bb2a9e03778
vladkorosko <vlad.korosko1@gmail.com>
2023-05-06 10:22:27 +0300

    Refactoring

--- video_processing/subvideo.py
+++ video_processing/subvideo.py
@@ -2,13 +2,17 @@ import asyncio
 
 
 async def subvideo(command, file_name):
-    ffmpeg_command = f'ffmpeg -ss {int(command[2])} -i {file_name} -to {int(command[2])} -c:v copy -c:a copy {"result_" + file_name}'
-    proc = await asyncio.create_subprocess_shell(
-        ffmpeg_command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
-    _, stderr = await proc.communicate()
-    if proc.returncode != 0:
-        error_message = stderr.decode().strip()
-        raise Exception(f'Error resizing video: {error_message}')
+    try:
+        ffmpeg_command = f'ffmpeg -ss {int(command[2])} -i {file_name} -to {int(command[2])} -c:v copy -c:a copy {"result_" + file_name}'
+        proc = await asyncio.create_subprocess_shell(
+            ffmpeg_command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+        _, stderr = await proc.communicate()
+        if proc.returncode != 0:
+            error_message = stderr.decode().strip()
+            raise Exception(f'Error resizing video: {error_message}')
+    except FileNotFoundError:
+        return 'Missing file'
+
